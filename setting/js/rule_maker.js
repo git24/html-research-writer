209936b8b59165a7f0e763c85b81a59e30bf3c66
Jace <ates8286@gmail.com>
2023-08-31 23:18:36 +0900

    입력 규칙 생성 화면 작업

--- setting/js/rule_maker.js
+++ setting/js/rule_maker.js
@@ -1,6 +1,28 @@
 const rm_main = {
 
+    createTable : () => {
+        // TODO: 입력한 항목수대로 테이블 생성        
+    },
+
+    initTable : () => {
+        // TODO: 테이블 초기화
+    },
+
+    appendRow : () => {
+        // TODO: 행 추가
+    },
+
     createRuleFile : rule => {    
+        if (!rule) {
+            alert("설정 파일 저장 실패");
+            return;
+        }
+
+        if (typeof rule !== 'object') {
+            alert("설정 파일 저장 실패");
+            return;
+        }
+
         const jsonCode = "const ri_rule = " + JSON.stringify(rule, null, 2);
         const blob = new Blob([jsonCode], { type: "text/javascript" });
         saveAs(blob, "rule.js");
